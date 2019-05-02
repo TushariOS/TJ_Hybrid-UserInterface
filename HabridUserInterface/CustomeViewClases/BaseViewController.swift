994a2fb6b210cb2341d67631f2097a69cb7da907
jjtushar <tjaunjalkar@gmail.com>
2019-05-02 11:43:12 +0530

    First comment

--- HabridUserInterface/CustomeViewClases/BaseViewController.swift
+++ HabridUserInterface/CustomeViewClases/BaseViewController.swift
@@ -14,7 +14,7 @@ class BaseViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = Colors.darkGreay
+        view.backgroundColor = .white
     }
 }
 
